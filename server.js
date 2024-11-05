2f39032d4dcc584f4fa4bf91ce9cf3d9aee41a37
pyneninarendra <pyneninarendra@gmail.com>
2024-11-05 02:24:36 -0500

    nodemon installation

--- server.js
+++ server.js
@@ -9,6 +9,5 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('App running on Host: ' + host);
   console.log('App running on port ' + port);
 });
