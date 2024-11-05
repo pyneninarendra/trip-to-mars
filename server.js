e60cc2ba69745e6014c7bee66102439e408bc88f
pyneninarendra <pyneninarendra@gmail.com>
2024-11-05 02:12:40 -0500

    initial commit

--- server.js
+++ server.js
@@ -9,5 +9,6 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
+  console.log('App running on Host: ' + host);
   console.log('App running on port ' + port);
 });
